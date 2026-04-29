ff00fd86ee6fed35415de1d53f96abaaef0d4b90
workfromanywheresa-max <workfromanywhere.sa@gmail.com>
2026-04-29 22:21:29 +0200

    Update views2.js

--- views2.js
+++ views2.js
@@ -42,6 +42,25 @@ embedModal.onclick = (e) => {
   }
 };
 
+document.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("copyBtn")) {
+
+    const btn = e.target;
+    const textarea = btn.parentElement.querySelector("textarea");
+
+    if (!textarea) return;
+
+    try {
+      await navigator.clipboard.writeText(textarea.value);
+
+      // nothing changes visually anymore
+
+    } catch (err) {
+      console.error("Copy failed:", err);
+    }
+  }
+});
+
 /* ---------------- TEST MODE ---------------- */
 const TEST_MODE = localStorage.getItem("testMode") === "true";
 
