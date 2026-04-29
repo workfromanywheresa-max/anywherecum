180791c36935629961c9fa2b3daa973a509b01a8
workfromanywheresa-max <workfromanywhere.sa@gmail.com>
2026-04-29 18:23:20 +0200

    Update views2.js

--- views2.js
+++ views2.js
@@ -353,7 +353,7 @@ video.qualities.forEach(q => {
 
   link.style.display = "block";
   link.style.color = "#ff4444";
-  link.style.fontSize = "12px";
+  link.style.fontSize = "8px";
   link.style.wordBreak = "break-all";
 
   link.onclick = () => {
