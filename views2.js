545165796ce39743640fcbd0b419d39e97a5c2dc
workfromanywheresa-max <workfromanywhere.sa@gmail.com>
2026-04-30 14:32:40 +0200

    Update views2.js

--- views2.js
+++ views2.js
@@ -663,10 +663,19 @@ likeBtn.style.display = "flex";
 likeBtn.style.alignItems = "center";
 likeBtn.style.justifyContent = "center";
 likeBtn.style.cursor = "pointer";
-likeBtn.style.border = "1px solid white";
-likeBtn.style.borderRadius = "50%";
-likeBtn.style.padding = "4px";
 
+likeBtn.style.display = "flex";
+likeBtn.style.alignItems = "center";
+likeBtn.style.gap = "6px";
+
+likeBtn.style.border = "1px solid white";
+likeBtn.style.borderRadius = "6px";
+likeBtn.style.padding = "6px 10px";
+
+likeBtn.style.cursor = "pointer";
+likeBtn.style.userSelect = "none";
+likeBtn.style.background = "transparent";
+  
 /* YOUR SVG */
 likeBtn.innerHTML = `
 <svg xmlns="http://www.w3.org/2000/svg"
